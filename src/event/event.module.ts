2f06e70621be53185f85797e832ae3500939f96b
Grygorii Lysytsia <lisitsa-gp@ranepa.ru>
2024-07-25 11:41:54 +0300

    add email remind sender

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -3,10 +3,11 @@ import { EventService } from './event.service';
 import { EventController } from './event.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { s3StorageService } from 'src/s3-storage/s3-storage.service';
+import { MailService } from 'src/mail/mail.service';
 
 @Module({
   imports: [PrismaModule],
-  providers: [EventService, s3StorageService],
+  providers: [EventService, s3StorageService, MailService],
   controllers: [EventController],
   exports: [EventService],
 })
--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { createTransport, Transporter } from 'nodemailer';
 import * as hbs from 'nodemailer-express-handlebars';
 import * as path from 'path';
+import { scheduleJob } from 'node-schedule';
 
 @Injectable()
 export class MailService {
@@ -40,10 +41,28 @@ export class MailService {
         template,
         context,
       });
-      console.log('Email sent successfully');
     } catch (error) {
       console.error('Error sending email:', error);
       throw new Error('Failed to send email');
     }
+  }
+
+  scheduleEmail(
+    to: string,
+    subject: string,
+    template: string,
+    context: any,
+    eventDate: Date,
+  ): void {
+    const reminderDate = new Date(eventDate);
+    reminderDate.setDate(reminderDate.getDate() - 7);
+
+    scheduleJob(reminderDate, () => {
+      this.sendEmail(to, subject, template, context)
+        .then(() => console.log('Email scheduled and sent successfully.'))
+        .catch((error) =>
+          console.error('Failed to send scheduled email:', error),
+        );
+    });
   }
 }
